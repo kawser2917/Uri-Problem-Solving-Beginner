e30ebef1caef7fb308d3e864bc4ac2dc17a263af
kawser2917 <kawserahmed8492@gmail.com>
2024-02-03 09:03:56 +0600

    1101 updated

--- 1101.py
+++ 1101.py
@@ -1,14 +1,14 @@
 while(True):
     try:
         a,b=list(map(int,input().split()))
-        x = min(a,b)
-        y = max(a,b)
+        if(a>b):
+            a,b=b,a
         if(a<=0 or b<=0):
             break
         else:
             sum=0
             result = ''
-            for i in range(x,y+1):
+            for i in range(a,b+1):
                 result += str(i)+' '
                 sum = sum + i
             result+= 'Sum=%d'
