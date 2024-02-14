612ddf81bdf6cbb225c7998ca259f532d883be23
kawser2917 <kawserahmed8492@gmail.com>
2024-02-14 08:22:08 +0600

    1173 solved

--- 1172.py
+++ 1172.py
@@ -2,4 +2,4 @@ for i in range(10):
     n = int(input())
     if(n<1):
         n =1
-    print(f"X[{i}] = {n}")
+    print(f"N[{i}] = {n}")
---
+++ 1173.py
@@ -0,0 +1,7 @@
+n = int(input())
+for i in range(10):
+    if(i==0):
+        n=n
+    else:
+        n=n*2
+    print(f"N[{i}] = {n}")
