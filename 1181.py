15ddd1853287c28052bd295e7dc658b112ea12d0
kawser2917 <kawserahmed8492@gmail.com>
2024-02-19 10:20:54 +0600

    1181 old and new added

--- 1181.py
+++ 1181.py
@@ -1,3 +1,16 @@
+# l = int(input())
+# n= input()
+# s= 0
+# for i in range(12):
+#     for j in range(12):
+#         v = float(input())
+#         if (i == l):
+#             s += v
+
+# if (n == 'S'):
+#     print("%.1f" % s)
+# else:
+#     print("%.1f" % (s / 12.0)
 M = []
 l = int(input())
 op = input()
@@ -15,4 +28,3 @@ if(op.upper() == "S"):
     print(f"{sum:.1f}")
 else:
     print(f"{(sum/12):.1f}")
-    
