fed0a2c71900e5c3758a1b3aa132b806e8fafcad
kawser2917 <kawserahmed8492@gmail.com>
2024-02-19 11:28:22 +0600

    1184 solved

---
+++ 1184.py
@@ -0,0 +1,18 @@
+op = input()
+M = []
+for i in range(12):
+    element = []
+    for j in range(12):
+        v = float(input())
+        element.append(v)
+    M.append(element)
+sum = 0
+n = 1
+for k in range(12):
+    for l in range(n,12):
+        sum +=M[l][k]
+    n+=1
+if (op.upper()== "S"):
+    print(f"{sum:.1f}")
+elif(op.upper() == "M"):
+    print(f"{(sum/66.0):.1f}")
