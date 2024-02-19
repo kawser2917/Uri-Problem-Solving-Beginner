3e943d57fa5b8ba9b89ada932ca37482029f340e
kawser2917 <kawserahmed8492@gmail.com>
2024-02-19 10:19:55 +0600

    1181 updated

--- 1181.py
+++ 1181.py
@@ -1,13 +1,18 @@
+M = []
 l = int(input())
-n= input()
-s= 0
+op = input()
+sum = 0
 for i in range(12):
+    element = []
     for j in range(12):
         v = float(input())
-        if (i == l):
-            s += v
+        element.append(v)
+    M.append(element)
 
-if (n == 'S'):
-    print("%.1f" % s)
+for i in range(12):
+    sum += M[l][i]
+if(op.upper() == "S"):
+    print(f"{sum:.1f}")
 else:
-    print("%.1f" % (s / 12.0))
+    print(f"{(sum/12):.1f}")
+    
