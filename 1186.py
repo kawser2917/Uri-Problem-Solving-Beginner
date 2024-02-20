bc6fb42c752d00d1e9eb2f71fb25272a554aeec9
kawser2917 <kawserahmed8492@gmail.com>
2024-02-20 09:03:38 +0600

    1186 solved

--- 1186.py
+++ 1186.py
@@ -6,13 +6,13 @@ for i in range(12):
         v = float(input())
         element.append(v)
     M.append(element)
-sum = 0
+sum=0
 n=11
-for k in range(12):
-    for l in range(0,n):
-        sum +=M[k][l]
+for i in range(1,12):
+    for j in range(n,12):
+        sum+=M[i][j]
     n-=1
-if (op.upper() == "S"):
+if op.upper() == "S":
     print(f"{sum:.1f}")
-elif(op.upper() == "M"):
-    print(f"{(sum/66.0):.1f}")
+elif op.upper() == "M":
+    print(f"{(sum/66):.1f}")
